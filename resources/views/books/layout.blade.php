62f667499c4c275a7290a2d880a1a66ee9d54f43
josue.merino <josue.merino@alumnos.uneatlantico.es>
2021-03-31 12:38:20 +0200

    Adding design to all views

--- resources/views/books/layout.blade.php
+++ resources/views/books/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Laravel 5.6 CRUD Application</title>
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+	<link rel="shortcut icon" href="{{url('https://www.flaticon.com/svg/vstatic/svg/3534/3534033.svg?token=exp=1617185185~hmac=cfd122c585d960813b66fad088a1bbea')}}" type="image/x-icon">
 </head>
 <body>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="{{url('https://www.flaticon.com/svg/vstatic/svg/1243/1243420.svg?token=exp=1617184866~hmac=74b03d0ac363196d26c0fe4d2593fd06')}}" type="image/x-icon">
     <title>Menu</title>
 </head>
 <body>
-    <a href="{{route('news.index')}}">News</a>
-    <a href="{{route('books.index')}}">Books</a>
+
+    <header>
+        <nav>
+            <div class="nav-wrapper purple">
+                <a href="#" class="brand-logo center">Menú</a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <center>
+            <br />
+
+            <a class="waves-effect black btn-large" href="{{route('news.index')}}">News</a>
+    
+            <a class="waves-effect black btn-large" href="{{route('books.index')}}">Books</a>
+        </center>
+    </main>
+
+    
 </body>
 </html>
